258a3085a6dda24160c20ecae6f0f9dde364891e
Sam Crang <sam.crang@gmail.com>
2013-03-16 14:21:14 +0000

    more logging

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ var arDrone = require('ar-drone');
 var maxAltitude = 3.0;
 var minAltitude = 1.0;
 var client = arDrone.createClient();
-client.on('navdata', function () {});
+client.on('navdata', function () {
+  //console.log('navdata: %j', arguments);
+});
 
-[ 'navdata', 'landing', 'landed', 'takeoff', 'hovering', 'flying', 'lowBattery', 'batteryChange', 'altitudeChange' ].forEach(function (state) {
+[ 'navdata', 'landing', 'landed', 'takeoff', 'hovering', 'flying', 'lowBattery' ].forEach(function (state) {
     client.on(state, function(data) {
         //fs.
         //  console.log('event: %s', state);
@@ -18,11 +20,13 @@ client.on('batteryChange', function (e) {
 });
 
 client.on('altitudeChange', function (alt) {
+   console.log('altutudeChange')
     if (alt > maxAltitude) {
         console.log('Reached max altitude, going down!');
+        this.stop();
         this.clockwise(0.5);
         this.down(0.5);
-    } else {
+    } else if (alt < minAltitude) {
         console.log('Reached min altitude, going up!');
         this.counterClockwise(0.5);
         this.up(0.5);
@@ -34,9 +38,10 @@ console.log('Taking off: %s', client.takeoff());
 
 // start scanning
 client.after(2000, function startScanning() {
-    console.log('Starting sequence');
+   console.log('Starting sequence');
    this.up(0.5);
    this.after(1000 * 30, function () {
+       console.log('landing!');
        this.stop();
        this.land();
    });
