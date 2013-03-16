e1778af860c17d7a1c7911398b43756ff80a54b4
Sam Crang <sam.crang@gmail.com>
2013-03-16 10:44:07 +0000

    take off!

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-var drone = require('ar-drone');
+var arDrone = require('ar-drone');
+var client = arDrone.createClient();
 
+client.takeoff();
 
-console.dir(drone);
-
+client.after(5000, function() {
+  this.stop();
+  this.land();
+})
 
